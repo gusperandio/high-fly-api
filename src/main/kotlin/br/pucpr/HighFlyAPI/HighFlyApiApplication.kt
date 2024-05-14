9aa327296501e4818fc281c698b579431883bdde
Gustavo <gustavo.sperandio25@gmail.com>
2024-05-14 15:58:18 -0300

    First Commit

---
+++ src/main/kotlin/br/pucpr/HighFlyAPI/HighFlyApiApplication.kt
@@ -0,0 +1,11 @@
+package br.pucpr.HighFlyAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighFlyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HighFlyApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/pucpr/HighFlyAPI/HighFlyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.pucpr.HighFlyAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighFlyApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

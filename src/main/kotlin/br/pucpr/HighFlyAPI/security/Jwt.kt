2c32be3e48d4eed4394e167bd1a307e2ab9deb4d
Gustavo <gustavo.sperandio25@gmail.com>
2024-06-01 22:08:59 -0300

    JWT

---
+++ src/main/kotlin/br/pucpr/HighFlyAPI/security/Jwt.kt
@@ -0,0 +1,39 @@
+package br.pucpr.HighFlyAPI.security
+
+import br.pucpr.HighFlyAPI.users.User
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.jackson.io.JacksonSerializer
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.Date
+
+@Component
+class Jwt {
+    fun createToken(user: User): String =
+        UserToken(user).let {
+            Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(SECRET.toByteArray())) //CREATE TOKEN
+                .json(JacksonSerializer()) //SERIALIZER
+                .issuedAt(utcNow().toDate()) //TOKEN WITH TIME NOW
+                .expiration(
+                    utcNow().plusHours(if (it.isAdmin) ADMIN_EXPIRE_HOURS else EXPIRE_HOURS).toDate()
+                ) //Expire Date TOKEN
+                .issuer(ISSUER) //! Server name
+                .subject(user.id.toString()) //The object how we apoint to discovery who is this guy
+                .claim(USER_FIELD, it) //Bonus Information
+                .compact() //GENERATE THE TEXT
+        }
+
+    companion object {
+        const val SECRET = "35981FB176D6262784986ED376677"
+        const val EXPIRE_HOURS = 1L;
+        const val ADMIN_EXPIRE_HOURS = 12L;
+        const val ISSUER = "High-Fly"
+        const val USER_FIELD = "user"
+
+        private fun utcNow() = ZonedDateTime.now(ZoneOffset.UTC)
+        private fun ZonedDateTime.toDate(): Date = Date.from(this.toInstant())
+    }
+}
---
+++ src/main/kotlin/br/pucpr/HighFlyAPI/security/UserToken.kt
@@ -0,0 +1,19 @@
+package br.pucpr.HighFlyAPI.security
+
+import br.pucpr.HighFlyAPI.users.User
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class UserToken(
+    val id: Long,
+    val name: String,
+    val roles: Set<String>
+){
+    constructor(): this(0, "", setOf())
+    constructor(user: User): this(
+        id = user.id!!,
+        name = user.name,
+        roles = user.roles.map{ it.name}.toSortedSet() )
+
+    @get:JsonIgnore
+    val isAdmin: Boolean get() = "ADMIN" in roles
+}
